70ab6ce2668e860ac87780cd966361a5b9ed1b82
Rishit <rishitpandey273@gmail.com>
2022-06-21 11:59:24 +0530

    commit

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,10 +3,12 @@ import Note from "./note.jsx";
 import Head from "./header.jsx";
 import Foot from "./footer.jsx";
 import notes from "../notes.js";
+import CreateArea from "./CreateArea.jsx";
 function App() {
   return (
     <div>
       <Head />
+      <CreateArea/>
       {notes.map(note=><Note key={note.key} name={note.title} content={note.content}/>)}
       <Foot />
     </div>
